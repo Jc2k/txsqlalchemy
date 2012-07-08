d051227878231aa8170152a506fd166d9bf29a1b
John Carr <john.carr@unrouted.co.uk>
2012-07-08 16:01:12 +0100

    Refactor filter tests to resuse a Fixture object

--- txsqlalchemy/tests/test_filter.py
+++ txsqlalchemy/tests/test_filter.py
@@ -3,71 +3,51 @@ from twisted.internet import defer
 from txsqlalchemy import Column, DateTime, String, Integer, model_base
 import datetime
 
-from .base import FixtureTestCase, TestCase
+from .base import FixtureTestCase, fixtures
 
-class TestFiltering(TestCase):
+class TestFiltering(FixtureTestCase):
 
-    @defer.inlineCallbacks
-    def setUpModel(self):
-        Base = model_base()
-        Base.bind("sqlite://")
-        class FooBar(Base):
-            id = Column(Integer, primary_key=True)
-            name = Column(String)
-            date = Column(DateTime)
-        yield FooBar.create()
-        yield FooBar.insert(name = "John", date=datetime.date(year=1900, month=1, day=12))
-        yield FooBar.insert(name = "Alex", date=datetime.date(year=2005, month=1, day=1))
-        yield FooBar.insert(name = "Timothy", date=datetime.date(year=1900, month=5, day=1))
-        yield FooBar.insert(name = "Peter", date=datetime.date(year=1900, month=3, day=5))
-        defer.returnValue(FooBar)
+    __fixture__ = fixtures.FooBarBaz
 
     @defer.inlineCallbacks
     def test_simple(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(name = "John").select()
+        results = yield self.FooBar.objects.filter(name = "John").select()
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0].name, "John")
 
     @defer.inlineCallbacks
     def test_exact(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(name__exact="John").select()
+        results = yield self.FooBar.objects.filter(name__exact="John").select()
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0].name, "John")
 
     @defer.inlineCallbacks
     def test_day(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(date__day = 5).select()
+        results = yield self.FooBar.objects.filter(date__day = 5).select()
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0].name, "Peter")
 
     @defer.inlineCallbacks
     def test_month(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(date__month = 5).select()
+        results = yield self.FooBar.objects.filter(date__month = 5).select()
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0].name, "Timothy")
 
     @defer.inlineCallbacks
     def test_year(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(date__year = 2005).select()
+        results = yield self.FooBar.objects.filter(date__year = 2005).select()
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0].name, "Alex")
 
     @defer.inlineCallbacks
     def test_week_date(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(date__week_date = 5).select()
+        results = yield self.FooBar.objects.filter(date__week_date = 5).select()
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0].name, "John")
 
     @defer.inlineCallbacks
     def test_in(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(id__in=(1,2,3)).select()
+        results = yield self.FooBar.objects.filter(id__in=(1,2,3)).select()
         self.assertEqual(len(results), 3)
         self.assertEqual(results[0].name, "John")
         self.assertEqual(results[1].name, "Alex")
@@ -75,56 +55,48 @@ class TestFiltering(TestCase):
 
     @defer.inlineCallbacks
     def test_contains(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(name__contains='oh').select()
+        results = yield self.FooBar.objects.filter(name__contains='oh').select()
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0].name, "John")
 
     #@defer.inlineCallbacks
     #def test_contains_is_case_sensitive(self):
-    #    FooBar = yield self.setUpModel()
-    #    results = yield FooBar.objects.filter(name__contains='OH').select()
+    #    results = yield self.FooBar.objects.filter(name__contains='OH').select()
     #    self.assertEqual(len(results), 0)
 
     @defer.inlineCallbacks
     def test_icontains(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(name__icontains='OH').select()
+        results = yield self.FooBar.objects.filter(name__icontains='OH').select()
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0].name, "John")
 
     @defer.inlineCallbacks
     def test_startswith(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(name__startswith = "J").select()
+        results = yield self.FooBar.objects.filter(name__startswith = "J").select()
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0].name, "John")
 
     @defer.inlineCallbacks
     def test_istartswith(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(name__istartswith = "j").select()
+        results = yield self.FooBar.objects.filter(name__istartswith = "j").select()
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0].name, "John")
 
     @defer.inlineCallbacks
     def test_endswith(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(name__endswith = "n").select()
+        results = yield self.FooBar.objects.filter(name__endswith = "n").select()
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0].name, "John")
 
     @defer.inlineCallbacks
     def test_iendswith(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(name__iendswith = "N").select()
+        results = yield self.FooBar.objects.filter(name__iendswith = "N").select()
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0].name, "John")
 
     @defer.inlineCallbacks
     def test_range(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(id__range = (0, 20)).select()
+        results = yield self.FooBar.objects.filter(id__range = (0, 20)).select()
         self.assertEqual(len(results), 4)
         self.assertEqual(results[0].name, "John")
         self.assertEqual(results[1].name, "Alex")
@@ -133,72 +105,50 @@ class TestFiltering(TestCase):
 
     @defer.inlineCallbacks
     def test_isnull(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(date__isnull = True).select()
+        results = yield self.FooBar.objects.filter(date__isnull = True).select()
         self.assertEquals(len(results), 0)
 
     @defer.inlineCallbacks
     def test_notisnull(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(date__isnull = False).select()
+        results = yield self.FooBar.objects.filter(date__isnull = False).select()
         self.assertEquals(len(results), 4)
 
 
-class TestChain(TestCase):
+class TestChain(FixtureTestCase):
 
-    @defer.inlineCallbacks
-    def setUpModel(self):
-        Base = model_base()
-        Base.bind("sqlite://")
-        class FooBar(Base):
-            id = Column(Integer, primary_key=True)
-            name = Column(String)
-            date = Column(DateTime)
-        yield FooBar.create()
-        yield FooBar.insert(name = "John", date=datetime.date(year=1900, month=1, day=12))
-        yield FooBar.insert(name = "Alex", date=datetime.date(year=2005, month=1, day=1))
-        defer.returnValue(FooBar)
+    __fixture__ = fixtures.FooBarBaz
 
     @defer.inlineCallbacks
     def test_filter_then_exclude(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(id__range = (0, 20)).exclude(id=2).select()
-        self.assertEqual(len(results), 1)
+        results = yield self.FooBar.objects.filter(id__range = (0, 20)).exclude(id=2).select()
+        self.assertEqual(len(results), 3)
         self.assertEqual(results[0].name, "John")
 
     @defer.inlineCallbacks
     def test_exclude_then_filter(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.exclude(id=1).filter(id__range = (0, 20)).select()
-        self.assertEqual(len(results), 1)
+        results = yield self.FooBar.objects.exclude(id=1).filter(id__range = (0, 20)).select()
+        self.assertEqual(len(results), 3)
         self.assertEqual(results[0].name, "Alex")
 
     @defer.inlineCallbacks
     def test_filter_then_filter(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.filter(id=2).filter(id__range = (0, 20)).select()
+        results = yield self.FooBar.objects.filter(id=2).filter(id__range = (0, 20)).select()
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0].name, "Alex")
 
     @defer.inlineCallbacks
     def test_exclude_then_exclude(self):
-        FooBar = yield self.setUpModel()
-        results = yield FooBar.objects.exclude(id=1).exclude(id__range = (0, 20)).select()
+        results = yield self.FooBar.objects.exclude(id=1).exclude(id__range = (0, 20)).select()
         self.assertEqual(len(results), 0)
 
 
-class TestCount(TestCase):
+class TestCount(FixtureTestCase):
+
+    __fixture__ = fixtures.FooBarBaz
 
     @defer.inlineCallbacks
     def test_case(self):
-        Base = model_base()
-        Base.bind("sqlite://")
-        class FooBar(Base):
-            id = Column(Integer, primary_key=True)
-        yield FooBar.create()
-        yield FooBar.insert()
-        yield FooBar.insert()
-        count = yield FooBar.objects.count()
-        self.assertEquals(count, 2)
+        count = yield self.FooBar.objects.count()
+        self.assertEquals(count, 4)
 
 
